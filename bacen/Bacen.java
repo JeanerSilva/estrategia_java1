0b1533a5b486781dcba9be4686c36961458e4149
JeanerSilva <jeanerufmg@gmail.com>
2024-02-03 16:08:58 -0300

    Reposicionamento de direitórios e docker

---
+++ bacen/Bacen.java
@@ -0,0 +1,9 @@
+//package bacen;
+
+public class Bacen {
+    public static void main(String args[]) {
+        System.out.println("hello===================================================");
+        double salarioNovo = 1, salarioAtual = 10.1;
+        System.out.println("Salário novo " + salarioNovo + ". Salário atual " + salarioAtual);
+    }
+}
